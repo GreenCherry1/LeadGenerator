9beeacc4a225db89a0c074d575a8f78ebfc06385
GreenCherry1 <137512652+GreenCherry1@users.noreply.github.com>
2023-07-04 19:54:44 +0300

    Update LeadGenerator.py

--- LeadGenerator.py
+++ LeadGenerator.py
@@ -7,11 +7,10 @@ import openpyxl
 import requests
 import json
 
+
 class Scraper:
-    def __init__(self):
-        return self.driver = webdriver.Chrome()
-	
-    def scroll(self, driver):
+    @staticmethod
+    def scroll(driver):
         """
         Perform scrolling action on the web page.
 
@@ -25,7 +24,8 @@ class Scraper:
             .scroll_from_origin(scroll_origin, 0, 100) \
             .perform()
 
-    def close_tab(self, driver):
+    @staticmethod
+    def close_tab(driver):
         """
         Close the current tab and switch to the last tab.
 
@@ -35,14 +35,14 @@ class Scraper:
         driver.close()
         driver.switch_to.window(driver.window_handles[-1])
 
-    def get_email_page(self, driver, pages, HasPhone):
+    def get_email_page(self, driver, pages, has_phone):
         """
         Extract email addresses from web pages.
 
         Args:
             driver (WebDriver): The Selenium WebDriver instance.
             pages (list): List of URLs to visit.
-            HasPhone (bool): Flag indicating if phone number is already found.
+            has_phone (bool): Flag indicating if phone number is already found.
 
         Returns:
             dict: Dictionary containing the email and phone number found on the page.
@@ -53,7 +53,7 @@ class Scraper:
             try:
                 code = requests.get(page, headers=headers).status_code
             except Exception as ex_:
-                print(ex)
+                print(ex_)
                 continue
             if code != 200:
                 continue
@@ -61,27 +61,27 @@ class Scraper:
             driver.implicitly_wait(10)
             driver.switch_to.window(driver.window_handles[-1])
             links = driver.find_elements(By.TAG_NAME, 'a')
-            for i in links:
+            for link in links:
                 try:
-                    if "facebook" in i.get_attribute('href'):
-                        email = self.get_email_facebook(driver, i.get_attribute('href'))
+                    if "facebook" in link.get_attribute('href'):
+                        email = self.get_email_facebook(driver, link.get_attribute('href'))
                         if email:
                             self.close_tab(driver)
                             out['email'] = email
-                            if HasPhone:
+                            if has_phone:
                                 return out
                 except Exception as ex_:
                     print(ex_)
                     continue
                 try:
-                    if not HasPhone and 'tel' in i.get_attribute("href"):
-                        out['phone'] = i.get_attribute("href")[4:]
+                    if not has_phone and 'tel' in link.get_attribute("href"):
+                        out['phone'] = link.get_attribute("href")[4:]
                         if out['email']:
                             self.close_tab(driver)
                             return out
-                    if 'mailto' in i.get_attribute("href"):
-                        out['email'] = i.get_attribute("href")[7:]
-                        if HasPhone or out['phone']:
+                    if 'mailto' in link.get_attribute("href"):
+                        out['email'] = link.get_attribute("href")[7:]
+                        if has_phone or out['phone']:
                             self.close_tab(driver)
                             return out
                 except Exception as ex_:
@@ -89,7 +89,8 @@ class Scraper:
             self.close_tab(driver)
         return out
 
-    def log_in_facebook(self, driver):
+    @staticmethod
+    def log_in_facebook(driver):
         """
         Log in to Facebook.
 
@@ -120,80 +121,89 @@ class Scraper:
         driver.implicitly_wait(10)
         driver.switch_to.window(driver.window_handles[-1])
         self.log_in_facebook(driver)
-        info = driver.find_elements(By.XPATH,"//span[@class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h']")
-        for i in info:
-            text = i.text
+        info = driver.find_elements(By.XPATH,
+                                    "//span[@class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv "
+                                    "xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen "
+                                    "xo1l8bm xzsf02u x1yc453h']")
+        for information in info:
+            text = information.text
             if '@' in text:
                 self.close_tab(driver)
                 return text
         self.close_tab(driver)
         return ''
-    def run_scraper(self, search, Least_Leads):
+
+    def run_scraper(self, search, least_leads):
         """
         Run the web scraper.
 
         Args:
-            Search (str): The search query.
-            Least_Leads (int): The minimum number of leads to be scraped.
+            search (str): The search query.
+            least_leads (int): The minimum number of leads to be scraped.
         """
-        Soc_networking_list = [
+        soc_networking_list = [
             "facebook", "instagram", "glovo", "t.me/", "tiktok", "twitter", "reddit", "tumblr", "flickr", "instagtam"
         ]
         wb = openpyxl.Workbook()
         ws = wb.active
         driver = webdriver.Chrome()
-        driver.get("https://www.google.com.ua/maps/search/" + Search + '?hl=en')
-        Full_Leads = 0
+        driver.get("https://www.google.com.ua/maps/search/" + search + '?hl=en')
+        full_leads = 0
         links, used_links = [], 0
-        while Full_Leads < Least_Leads:
+        while full_leads < least_leads:
             self.scroll(driver)
             used_links += len(links)
             WebDriverWait(driver, timeout=10).until(lambda d: len(d.find_elements(By.CLASS_NAME, 'hfpxzc')) > used_links
-            )
-            links = [i.get_attribute('href') for i in driver.find_elements(By.CLASS_NAME, 'hfpxzc')[used_links:]]
+                                                    )
+            links = [business_page.get_attribute('href') for business_page in
+                     driver.find_elements(By.CLASS_NAME, 'hfpxzc')[used_links:]]
             if not len(links):
                 print('no more places')
                 break
-            for i in links:
-                driver.execute_script("window.open('%s', '_blank')" % i)
+            for link in links:
+                driver.execute_script("window.open('%s', '_blank')" % link)
                 driver.implicitly_wait(10)
                 driver.switch_to.window(driver.window_handles[-1])
                 pictures_buttons = driver.find_elements(By.CLASS_NAME, 'CsEnBe')
                 pictures_aria_label = [x.get_attribute("aria-label") for x in pictures_buttons]
-                websites = list(set([x.get_attribute('href') for x in pictures_buttons if x.tag_name == 'a' and x.get_attribute('aria-label') != 'Claim this business']))
-                own_websites = [website for website in websites if sum([int(socnetwork in website) for socnetwork in Soc_networking_list]) == 0 and website[-3:] != 'pdf']
+                websites = list(set([x.get_attribute('href') for x in pictures_buttons if
+                                     x.tag_name == 'a' and x.get_attribute('aria-label') != 'Claim this business']))
+                own_websites = [website for website in websites if sum([int(socnetwork in website) for socnetwork in
+                                                                        soc_networking_list]) == 0 and website[
+                                                                                                       -3:] != 'pdf']
                 if not len(websites):
                     self.close_tab(driver)
                     continue
-                HasPhone = False
+                has_phone = False
                 for pic in pictures_aria_label:
                     if "Plus code" in pic:
-                        ws.cell(row=Full_Leads + 1, column=1).value = pic[11:]
+                        ws.cell(row=full_leads + 1, column=1).value = pic[11:]
                     if "Phone" in pic:
-                        ws.cell(row=Full_Leads + 1, column=3).value = pic[6:]
-                        HasPhone = True
+                        ws.cell(row=full_leads + 1, column=3).value = pic[6:]
+                        has_phone = True
                 email = {'email': '', 'phone': ''}
                 if len(own_websites):
-                    email = self.get_email_page(driver, own_websites, HasPhone)
-                    ws.cell(row=Full_Leads + 1, column=2).value = ' '.join(own_websites)
+                    email = self.get_email_page(driver, own_websites, has_phone)
+                    ws.cell(row=full_leads + 1, column=2).value = ' '.join(own_websites)
                 else:
-                    ws.cell(row=Full_Leads + 2, column=2).value = ' '.join(websites)
+                    ws.cell(row=full_leads + 2, column=2).value = ' '.join(websites)
                     for website in websites:
                         if 'facebook' in website:
                             email['email'] = self.get_email_facebook(driver, website)
                             break
                 if email['phone']:
-                    ws.cell(row=Full_Leads + 1, column=3).value = email['phone']
-                    HasPhone = True
-                if HasPhone and email['email']:
-                    ws.cell(row=Full_Leads + 1, column=4).value = email['email']
-                    Full_Leads += 1
+                    ws.cell(row=full_leads + 1, column=3).value = email['phone']
+                    has_phone = True
+                if has_phone and email['email']:
+                    ws.cell(row=full_leads + 1, column=4).value = email['email']
+                    full_leads += 1
                 print(email)
                 self.close_tab(driver)
-                if Full_Leads == Least_Leads:
+                if full_leads == Least_Leads:
                     break
         wb.save("Leads.xlsx")
         driver.quit()
+
 
 with open("settings.json") as jsonfile:
     data = json.load(jsonfile)
